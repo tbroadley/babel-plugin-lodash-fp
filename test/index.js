c2adc8f5a87c14eb5cf1d816b7f521629f89ff1c
Thomas Broadley <buriedunderbooks@hotmail.com>
2016-07-02 01:55:40 +0100

    extracted a function to call expect

--- test/index.js
+++ test/index.js
@@ -5,8 +5,13 @@ function plugin(str) {
   return transform(str, { plugins: ['./index'] }).code;
 }
 
+function test(toTest, expected) {
+  return () => expect(plugin(toTest)).to.equal(expected);
+}
+
 describe('babel-plugin-lodash-fp', () => {
-  it('reverses an identifier', () => {
-    expect(plugin('var asdf;')).to.equal('var fdsa;');
-  });
+  it('reverses an identifier', test(
+    'var asdf;',
+    'var fdsa;'
+  ));
 });
