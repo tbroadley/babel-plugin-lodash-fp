034482434c270c00fdfae2f129f46785f8ecdfac
Thomas Broadley <buriedunderbooks@hotmail.com>
2016-10-26 13:12:05 +0100

    added comments and changed formatting in build-call

--- src/helpers/build-call.js
+++ src/helpers/build-call.js
@@ -8,16 +8,23 @@ import setReplaced from './set-replaced';
 export default (fn, args) => {
   const fnName = getPropertyName(fn);
 
+  // Look up the arity of the given function.
   let fnArity = _.flow(
-    _.map(_.flow(
-      _.toString,
-      _.get(_, m.aryMethod),
-      _.includes(fnName))
+    _.map(
+      _.flow(
+        _.toString,
+        _.get(_, m.aryMethod),
+        _.includes(fnName)
+      )
     ),
     _.findIndex(_.identity),
     _.add(1)
   )(_.range(1, 5));
 
+  /*
+   * fnRearg is a permutation of [1..fnArity] that describes how the arguments
+   * of the function are reordered when translating from lodash to lodash/fp.
+   */
   let fnRearg;
   if (fnArity === 0) {
     fnRearg = _.range(0, args.length);
@@ -29,6 +36,10 @@ export default (fn, args) => {
     fnRearg = m.methodRearg[fnName] || m.aryRearg[fnArity];
   }
 
+  /*
+   * If this is a partial function application, the arguments that were not
+   * passed are marked with undefined.
+   */
   const augmentedArgs = _.concat(
     args, _.times(
       () => t.identifier('undefined'),
@@ -36,6 +47,7 @@ export default (fn, args) => {
     )
   );
 
+  // Reorder the arguments and drop placeholders from the right end.
   const newArgs = _.flow(
     _.map(index => augmentedArgs[_.indexOf(index)(fnRearg)]),
     _.dropRightWhile(_.flow(_.get('name'), _.isEqual('_')))
@@ -44,8 +56,11 @@ export default (fn, args) => {
   if (_.isEmpty(newArgs)) {
     return fn;
   } else {
-    return setReplaced(t.callExpression(fn,
-      _.dropRightWhile(_.flow(_.get('name'), _.isEqual('undefined')))(newArgs)
-    ));
+    return setReplaced(
+      t.callExpression(
+        fn,
+        _.dropRightWhile(_.flow(_.get('name'), _.isEqual('undefined')))(newArgs)
+      )
+    );
   }
 };
